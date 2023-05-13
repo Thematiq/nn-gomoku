8fe24dead7399e7193eb0c8b417fa187b49d0c02
Maksymilian Wojnar <38166640+m-wojnar@users.noreply.github.com>
2023-05-13 11:11:34 +0200

    DQN (#3)
    * Add first draft of DQN
    
    * Fix DQN

--- agents/agent.py
+++ agents/agent.py
@@ -1,21 +1,23 @@
 from abc import ABC, abstractmethod
 
+import numpy as np
+
 
 class Agent(ABC):
     @abstractmethod
-    def update(self, state, action, reward, next_state, terminal):
+    def update(self, state: np.ndarray, action: int, reward: float, next_state: np.ndarray, terminal: bool) -> None:
         pass
 
     @abstractmethod
-    def act(self, state):
+    def act(self, state: np.ndarray, is_training=True) -> int:
         pass
 
-    def save(self, path):
+    def save(self, path: str) -> None:
         raise NotImplementedError()
 
     @staticmethod
-    def load(path):
+    def load(path: str) -> 'Agent':
         raise NotImplementedError()
 
-    def opponent_policy(self, state, *_):
+    def opponent_policy(self, state: np.ndarray, *_) -> int:
         return self.act(state)
